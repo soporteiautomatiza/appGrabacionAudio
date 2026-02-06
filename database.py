abda90e61f7976747e3c8fdd1928339da49d5c41
Developer <dev@iautomatiza.net>
2026-02-06 07:41:38 +0100

    Update database.py

--- database.py
+++ database.py
@@ -2,33 +2,133 @@ import streamlit as st
 import os
 from datetime import datetime
 import httpx
-from postgrest import AsyncPostgrestClient
+import json
 
-# Cliente Supabase simplificado usando postgrest directamente
-class SupabaseClient:
+class SimpleSupabaseClient:
+    """Cliente Supabase simplificado usando httpx directamente"""
+    
     def __init__(self, url: str, key: str):
-        self.url = url
+        self.url = url.rstrip('/')
         self.key = key
-        self.rest_url = url.rstrip('/') + '/rest/v1'
+        self.rest_url = f'{self.url}/rest/v1'
         self.headers = {
             'apikey': key,
             'Authorization': f'Bearer {key}',
             'Content-Type': 'application/json'
         }
-        self._http_client = httpx.Client()
+        self._client = httpx.Client()
     
     def table(self, table_name: str):
-        """Retorna un cliente PostgREST para una tabla específica"""
-        return AsyncPostgrestClient(
-            self.rest_url,
-            table_name=table_name,
-            headers=self.headers,
-            http_client=self._http_client
-        )
+        """Retorna un objeto que permite hacer queries a una tabla"""
+        return TableClient(self, table_name)
+
+class TableClient:
+    """Cliente para una tabla específica"""
+    
+    def __init__(self, db: SimpleSupabaseClient, table_name: str):
+        self.db = db
+        self.table_name = table_name
+        self.url = f'{db.rest_url}/{table_name}'
+        self.query_params = {}
+        self.select_cols = '*'
+        self.order_by = None
+    
+    def select(self, columns='*'):
+        """SELECT columnas"""
+        self.select_cols = columns
+        return self
+    
+    def order(self, column, desc=False):
+        """ORDER BY"""
+        order_dir = 'desc' if desc else 'asc'
+        self.order_by = f'{column}.{order_dir}'
+        return self
+    
+    def eq(self, column, value):
+        """WHERE column = value"""
+        self.query_params[column] = f'eq.{value}'
+        return self
+    
+    def execute(self):
+        """Ejecuta la query"""
+        try:
+            params = {'select': self.select_cols}
+            params.update(self.query_params)
+            
+            if self.order_by:
+                params['order'] = self.order_by
+            
+            response = self.db._client.get(self.url, headers=self.db.headers, params=params)
+            response.raise_for_status()
+            
+            # Retornar un objeto con atributo .data
+            class Response:
+                def __init__(self, data):
+                    self.data = data
+            
+            return Response(response.json())
+        
+        except Exception as e:
+            # Retornar respuesta vacía en caso de error
+            class Response:
+                def __init__(self):
+                    self.data = []
+            
+            return Response()
+    
+    def insert(self, data):
+        """INSERT datos"""
+        try:
+            response = self.db._client.post(self.url, headers=self.db.headers, json=data)
+            response.raise_for_status()
+            
+            class Response:
+                def __init__(self, data):
+                    self.data = data if isinstance(data, list) else [data]
+            
+            return Response(response.json())
+        except:
+            class Response:
+                def __init__(self):
+                    self.data = []
+            
+            return Response()
+    
+    def delete(self):
+        """DELETE"""
+        return DeleteClient(self)
+
+class DeleteClient:
+    def __init__(self, table_client: TableClient):
+        self.table_client = table_client
+    
+    def eq(self, column, value):
+        self.table_client.query_params[column] = f'eq.{value}'
+        return self
+    
+    def execute(self):
+        """Ejecuta el delete"""
+        try:
+            response = self.table_client.db._client.delete(
+                self.table_client.url,
+                headers=self.table_client.db.headers,
+                params=self.table_client.query_params
+            )
+            response.raise_for_status()
+            
+            class Response:
+                data = []
+            
+            return Response()
+        except:
+            class Response:
+                data = []
+            
+            return Response()
 
 def create_client(url: str, key: str):
     """Crea un cliente Supabase"""
-    return SupabaseClient(url, key)
+    return SimpleSupabaseClient(url, key)
 
 @st.cache_resource
 def init_supabase() -> Client:
